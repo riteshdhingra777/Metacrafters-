72458a6bbda94e00896a3b8583f87898e63dbd3f
riteshdhingra777 <riteshdhingra777@gmail.com>
2024-05-31 19:51:39 +0530

    Update JSASSIGNMENT.js
    assignment

--- JSASSIGNMENT.js
+++ JSASSIGNMENT.js
@@ -1,24 +1,33 @@
-// Define an array to hold NFT objects
+/*
+Assessment Requirements
+1. Create a variable that can hold a number of NFT's. What type of variable might this be?
+2. Create an object inside your mintNFT function that will hold the metadata for your NFTs. 
+   The metadata values will be passed to the function as parameters. When the NFT is ready, 
+   you will store it in the variable you created in step 1
+3. Your listNFTs() function will print all of your NFTs metadata to the console (i.e. console.log("Name: " + someNFT.name))
+4. For good measure, getTotalSupply() should return the number of NFT's you have created
+*/
+
+// create a variable to hold your NFT's
 let Collection_of_nft = [];
 
-// Function to mint a new NFT and store its metadata
+// this function will take in some values as parameters, create an
+// NFT object using the parameters passed to it for its metadata, 
+// and store it in the variable above.
 function mintNFT(name, rarity, creator, dateCreated) {
-    // Create a new NFT object
     let nft = {
         name: name,
         rarity: rarity,
         creator: creator,
         dateCreated: dateCreated
     };
-    // Store the new NFT object in the collection
     Collection_of_nft.push(nft);
 }
 
-// Function to list all NFTs' metadata
+// create a "loop" that will go through an "array" of NFT's
+// and print their metadata with console.log()
 function listNFTs() {
-    // Loop through each NFT in the collection
     Collection_of_nft.forEach(function(nft) {
-        // Print metadata for each NFT
         console.log("Name: " + nft.name);
         console.log("Rarity: " + nft.rarity);
         console.log("Creator: " + nft.creator);
@@ -27,18 +36,16 @@ function listNFTs() {
     });
 }
 
-// Function to get the total number of minted NFTs
+// print the total number of NFTs we have minted to the console
 function getTotalSupply() {
     // Return the length of the NFT collection
     return Collection_of_nft.length;
 }
 
-// Demonstrate functionality by minting some NFTs and listing them
+// call your functions below this line
 mintNFT("Lion", "Very Rare", "Ritesh", "2024-01-25");
 mintNFT("Monkey", "Common", "Madhav", "2023-04-07");
 mintNFT("Horse", "Rare", "Aditya", "2024-02-10");
 
-// List all minted NFTs
 listNFTs();
-// Print the total number of minted NFTs
 console.log("Total NFTs minted: " + getTotalSupply());
